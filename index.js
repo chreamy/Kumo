cc61ada16f09594a20b576e02d7fcc2a4d8a01bb
kiyoyoneko <kiyoedm@gmail.com>
2022-10-19 17:29:59 -0500

    no

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
-const {Client,Events,GatewayIntentBits} = require('discord.js')
+//const {Client,Events,GatewayIntentBits} = require('discord.js')
+const Commando = require('discord.js-commando')
+const path = require('path')
 require('dotenv/config')
 const config = require('./config.json')
 const {command} = require('./command')
@@ -23,18 +25,27 @@ const spam = require('./cmds/spam')
 //         end cmd list
 //+------------------------------------------+
 
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-    ]
+const client = new Commando.CommandoClient({
+    owner: '532331965651877888',
+    commandPrefix: config.prefix,
+    invite: 'https://discord.gg/frECbWEpzR',
 })
+client.registry
+	.registerDefaultTypes()
+	//.registerGroups([
+	//	['first', 'Your First Command Group'],
+	//	['second', 'Your Second Command Group'],
+	//])
+	.registerDefaultGroups()
+	.registerDefaultCommands()
+	.registerCommandsIn(path.join(__dirname, 'cmds'));
 client.on('ready', () =>{
     console.log('bot ready')
-    for (const [key, value] of Object.entries(cmdlist)) {
-        command(client,value,eval(key))
-      }
+    console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
+	client.user.setActivity('with Commando');
+    //for (const [key, value] of Object.entries(cmdlist)) {
+    //    command(client,value,eval(key))
+     // }
 })
 
 
