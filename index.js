d9c2c668849aa68cdf8a7b2366ccc8efc2645097
kiyoyoneko <kiyoedm@gmail.com>
2022-10-20 05:09:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,9 @@ const client = new Client({
     ]
 })
 client.on('ready', () =>{
-    console.log('bot is ready lol')
+    console.log('bot is ready')
+    client.channels.fetch('1032042520089210953')
+    .then(channel=>channel.send('Back Online'))
     for (const [category,cat_commands] of Object.entries(cmdlist)) {
     for (const [key, value] of Object.entries(cat_commands)) {
         command(client,value,eval(key))
