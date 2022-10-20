b992476d45ad6605566db7ef72a782fec2efa9a3
kiyoyoneko <kiyoedm@gmail.com>
2022-10-20 01:03:03 -0500

    10/19 done

---
+++ test-schema.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const schema = new mongoose.Schema({
+    message:{
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('testing', schema)
