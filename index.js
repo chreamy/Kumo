c9db84686da7233b365bdc29ba14cad8a2cadab6
kiyoyoneko <kiyoedm@gmail.com>
2022-10-20 01:30:39 -0500

    Save

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-
+const mongoose = require('mongoose')
 const {Client,Events,GatewayIntentBits} = require('discord.js')
 require('dotenv/config')
 const config = require('./config.json')
 const {command} = require('./command')
+const testSchema = require('./test-schema') 
 //+------------------------------------------+
 //         cmd list
 let cmdlist = {
@@ -38,7 +39,11 @@ const client = new Client({
         GatewayIntentBits.MessageContent,
     ]
 })
-client.on('ready', () =>{
+client.on('ready', async () =>{
+    await mongoose.connect(process.env.MONGO_URI,{keepAlive: true})
+    await new testSchema({
+        message: 'hello world'
+    }).save()
     console.log('bot ready')
     for (const [category,cat_commands] of Object.entries(cmdlist)) {
     for (const [key, value] of Object.entries(cat_commands)) {
