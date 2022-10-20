3e0cd95a283716a81da12e2d96d08084a5f9ff52
kiyoyoneko <kiyoedm@gmail.com>
2022-10-20 03:32:26 -0500

    .

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-//const mongoose = require('mongoose')
+
 const {Client,Events,GatewayIntentBits} = require('discord.js')
 require('dotenv/config')
-//const config = require('./config.json')
+const config = require('./config.json')
 const {command} = require('./command')
-//const testSchema = require('./test-schema') 
 //+------------------------------------------+
 //         cmd list
 let cmdlist = {
@@ -26,7 +25,7 @@ const ping = require('./cmds/misc/ping')
 const help = require('./cmds/misc/help')
 const avatar = require('./cmds/misc/avatar')
 const servers = require('./cmds/misc/servers')
-const userinfo = require('./cmds/misc/userinfo') 
+const userinfo = require('./cmds/misc/userinfo')
 const spam = require('./cmds/fun/spam')
 const changeprefix = require('./cmds/util/changeprefix')
 //         end cmd list
@@ -39,12 +38,8 @@ const client = new Client({
         GatewayIntentBits.MessageContent,
     ]
 })
-client.on('ready', async () =>{
-    //await mongoose.connect(process.env.MONGO_URI,{keepAlive: true})
-    //await new testSchema({
-    //    message: 'hello world'
-    //}).save()
-    console.log('bot is ready')
+client.on('ready', () =>{
+    console.log('bot ready')
     for (const [category,cat_commands] of Object.entries(cmdlist)) {
     for (const [key, value] of Object.entries(cat_commands)) {
         command(client,value,eval(key))
