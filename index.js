a0c5e26a3259461247dae1a2d70cf8fee561b54f
kiyoyoneko <kiyoedm@gmail.com>
2022-10-20 01:09:10 -0500

    Revert "10/19 done"
    This reverts commit b992476d45ad6605566db7ef72a782fec2efa9a3.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const mongoose = require('mongoose')
+
 const {Client,Events,GatewayIntentBits} = require('discord.js')
 require('dotenv/config')
 const config = require('./config.json')
 const {command} = require('./command')
-const testSchema = require('./test-schema') 
 //+------------------------------------------+
 //         cmd list
 let cmdlist = {
@@ -39,11 +38,7 @@ const client = new Client({
         GatewayIntentBits.MessageContent,
     ]
 })
-client.on('ready', async () =>{
-    await mongoose.connect(process.env.MONGO_URI,{keepAlive: true})
-    await new testSchema({
-        message: 'hello world'
-    }).save()
+client.on('ready', () =>{
     console.log('bot ready')
     for (const [category,cat_commands] of Object.entries(cmdlist)) {
     for (const [key, value] of Object.entries(cat_commands)) {
