5f2ffb9e8bdae491abf96de865a817e52c6848be
kiyoyoneko <kiyoedm@gmail.com>
2022-10-20 01:22:24 -0500

    idk

--- command.js
+++ command.js
@@ -1,15 +1,22 @@
-const {prefix} = require('./config.json')
+const prefix = require('discord-prefix');
+let defaultPrefix = '!';
 module.exports.command = (client, aliases, callback) => {
     if(typeof aliases === 'string'){
         aliases = [aliases]
     }
     client.on('messageCreate',message =>{
+        if (!message.guild) return;
+        let guildPrefix = prefix.getPrefix(message.guild.id);
+        if (!guildPrefix) guildPrefix = defaultPrefix;
+        let args = message.content.slice(guildPrefix.length).split(' ');
+        args = args.filter(element => {if (Object.keys(element).length !== 0) {return true;}return false;})
+        if (!message.content.startsWith(guildPrefix)) return;
         const {content} = message;
         aliases.forEach(alias => {
-            const command = `${prefix}${alias}`
-            if(content.startsWith(`${command} `)||content===command){
-                let arg = `${content.slice(command.length)}`
-                let args = arg.split(" ").filter(element => {if (Object.keys(element).length !== 0) {return true;}return false;});
+            const command = `${alias}`
+            if(`${args[0]}`.toLowerCase()===`${command}`){
+                
+                args = args.slice(1)
                 callback(client,message,args)
             }
         })
--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-const mongoose = require('mongoose')
+
 const {Client,Events,GatewayIntentBits} = require('discord.js')
 require('dotenv/config')
 const config = require('./config.json')
 const {command} = require('./command')
-const testSchema = require('./test-schema') 
 //+------------------------------------------+
 //         cmd list
 let cmdlist = {
-    'ping':'ping',
-    'help':['help','h'],
-    'avatar':['ava','avatar'],
-    'servers':['servers','server'],
-    'userinfo':['userinfo','ui','info','getuser','gu'],
-    'spam':'spam'
+    'misc':{
+        'ping':'ping',
+        'help':['help','h'],
+        'avatar':['avatar','ava'],
+        'servers':['servers','server'],
+        'userinfo':['userinfo','ui','info','getuser','gu'],
+    },
+    'fun':{
+        'spam':'spam'
+    },
+    'util':{
+        'changeprefix':['changeprefix','prefix']
+    }
 }
 exports.cmdlist = cmdlist
-const ping = require('./cmds/ping')
-const help = require('./cmds/help')
-const avatar = require('./cmds/avatar')
-const servers = require('./cmds/servers')
-const userinfo = require('./cmds/userinfo')
-const spam = require('./cmds/spam')
+const ping = require('./cmds/misc/ping')
+const help = require('./cmds/misc/help')
+const avatar = require('./cmds/misc/avatar')
+const servers = require('./cmds/misc/servers')
+const userinfo = require('./cmds/misc/userinfo')
+const spam = require('./cmds/fun/spam')
+const changeprefix = require('./cmds/util/changeprefix')
 //         end cmd list
 //+------------------------------------------+
 
@@ -31,15 +38,13 @@ const client = new Client({
         GatewayIntentBits.MessageContent,
     ]
 })
-client.on('ready', async () =>{
-    await mongoose.connect(process.env.MONGO_URI,{keepAlive: true})
-    await new testSchema({
-        message: 'hello world'
-    }).save()
+client.on('ready', () =>{
     console.log('bot ready')
-    for (const [key, value] of Object.entries(cmdlist)) {
+    for (const [category,cat_commands] of Object.entries(cmdlist)) {
+    for (const [key, value] of Object.entries(cat_commands)) {
         command(client,value,eval(key))
       }
+    }
 })
 
 
