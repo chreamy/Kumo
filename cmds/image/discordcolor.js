4e76bce8034a4718ff37d6b5340288b74fd71b22
kiyoyoneko <kiyoedm@gmail.com>
2022-11-06 15:04:22 -0600

    attach file

--- cmds/image/discordcolor.js
+++ cmds/image/discordcolor.js
@@ -2,10 +2,12 @@ fetch = require('node-fetch')
 let desc = 'make pfp discord color'
 module.exports = async (client,message,args) => {
     const user = message.mentions.users.first() || message.author
-    //const files = await message.getImage();
-		//if (!Array.isArray(files)) return;
+    const attachment = message.attachments.first();
+    let url = attachment ? attachment.url : null;
+    if (!url){url = user.displayAvatarURL()}
+    const msg = await message.channel.send('**generating image...** *(Dependent on file size)*')
         try {
-			const json = await fetch(encodeURI(`https://nekobot.xyz/api/imagegen?type=blurpify&image=${user.displayAvatarURL()}`)).then(res => res.json());
+			const json = await fetch(encodeURI(`https://nekobot.xyz/api/imagegen?type=blurpify&image=${url}`)).then(res => res.json());
 
             const Embed = {
                 color: 0xFFFFFE,
@@ -16,5 +18,6 @@ module.exports = async (client,message,args) => {
 		} catch(err) {
 			if (message.deletable) message.delete();
 		}
+        msg.delete();
 }
 module.exports.desc = desc
