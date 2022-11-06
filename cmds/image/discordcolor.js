9d82d94fd80fedda49f5b8c5ec498328e79b834d
kiyoyoneko <kiyoedm@gmail.com>
2022-11-06 15:19:47 -0600

    file limit

--- cmds/image/discordcolor.js
+++ cmds/image/discordcolor.js
@@ -3,7 +3,13 @@ let desc = 'make pfp discord color'
 module.exports = async (client,message,args) => {
     const user = message.mentions.users.first() || message.author
     const attachment = message.attachments.first();
-    let url = attachment ? attachment.url : null;
+    let url = attachment ? attachment.url : null
+    if(attachment){
+        if(attachment.width>1000||attachment.height>1000){
+            message.channel.send('*Image too big (Max: 1000x1000)*')
+            return
+        }
+    }
     if (!url){url = user.displayAvatarURL()}
     const msg = await message.channel.send('**generating image...** *(Dependent on file size)*')
         try {
