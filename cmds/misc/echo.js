045d5fcbdfb75ecd80c41b67219e8e302549d6d2
chris <116124688+kiyoyoneko@users.noreply.github.com>
2023-06-11 12:32:45 -0500

    Update echo.js

--- cmds/misc/echo.js
+++ cmds/misc/echo.js
@@ -1,6 +1,8 @@
 let desc = 'Repeat Message'
 require('discord-reply');
 module.exports = async (client,message,args) => {
+    if(!message.author === '<@532331965651877888>')
+{return}
     let out = message.content.substr(message.content.indexOf(" ") + 1);
     if(message.reference){
         message.channel.send({ content: out, reply: { messageReference: message.reference.messageId },allowedMentions: { repliedUser: false }});
