a46a162e01ce8db0d07e6efa4f1a365b03c12787
Christopher Liu <116124688+chreamy@users.noreply.github.com>
2025-08-15 22:23:31 -0500

    limit channel

--- index.js
+++ index.js
@@ -14,28 +14,28 @@ const { joinguild } = require("./events/guildevents/joinguild");
 //+------------------------------------------+
 //         cmd list
 let cmdlist = {
-  // fun: {
-  //   spam: "spam",
-  // },
-  misc: {
-    // gpt: "gpt",
-    // ping: "ping",
-    echo: ["echo", "e"],
-    help: ["help", "h"],
-    // servers: ["servers", "server"]
-  },
-  yuna: {
-    genyuna: "genyuna",
-    yuna: ["yuna", "y"]
-  },
-  // user: {
-  //   avatar: ["avatar", "ava"],
-  //   userinfo: ["userinfo", "ui", "info", "getuser", "gu"],
-  //   messagecount: ["messagecount", "mc"],
-  // },
-  // util: {
-  //   changeprefix: ["changeprefix", "prefix"],
-  // },
+    // fun: {
+    //   spam: "spam",
+    // },
+    misc: {
+        // gpt: "gpt",
+        // ping: "ping",
+        echo: ["echo", "e"],
+        help: ["help", "h"],
+        // servers: ["servers", "server"]
+    },
+    yuna: {
+        genyuna: "genyuna",
+        yuna: ["yuna", "y"],
+    },
+    // user: {
+    //   avatar: ["avatar", "ava"],
+    //   userinfo: ["userinfo", "ui", "info", "getuser", "gu"],
+    //   messagecount: ["messagecount", "mc"],
+    // },
+    // util: {
+    //   changeprefix: ["changeprefix", "prefix"],
+    // },
 };
 exports.cmdlist = cmdlist;
 
@@ -58,88 +58,90 @@ const messagecount = require("./cmds/user/messagecountcmd");
 //+------------------------------------------+
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
 });
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true, // Use the new URL parser
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true, // Use the new URL parser
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("MongoDB connection error:", err));
 
 client.on("ready", async () => {
-  console.log("Bot is ready");
-  const currentdate = new Date();
-  const datetime = `${currentdate.getDate()}/${
-    currentdate.getMonth() + 1
-  }/${currentdate.getFullYear()} @ ${currentdate.getHours()}:${currentdate.getMinutes()}:${currentdate.getSeconds()}`;
-  const Embed = {
-    color: 0xfffffe,
-    description: `**Back Online ${datetime}**`,
-  };
-  client.channels
-    .fetch("1032596705201356810")
-    .then((channel) => channel.send({ embeds: [Embed] }));
+    console.log("Bot is ready");
+    const currentdate = new Date();
+    const datetime = `${currentdate.getDate()}/${
+        currentdate.getMonth() + 1
+    }/${currentdate.getFullYear()} @ ${currentdate.getHours()}:${currentdate.getMinutes()}:${currentdate.getSeconds()}`;
+    const Embed = {
+        color: 0xfffffe,
+        description: `**Back Online ${datetime}**`,
+    };
+    client.channels
+        .fetch("1032596705201356810")
+        .then((channel) => channel.send({ embeds: [Embed] }));
 });
 
 client.on("messageCreate", async (message) => {
-  if (!message.guild) return;
-  if (message.author === client.user) return;
-  if (message.author.bot) return;
-  const gptChannelId = "1328187329344442399";
-  const guilddb = await guildSchema.findOne({ _id: message.guild.id });
-  let guildPrefix = guilddb ? guilddb.prefix : defaultPrefix;
-  // Handle events for specific channel
-  if (
-    message.channel.id === gptChannelId &&
-    !message.content.startsWith(guildPrefix) &&
-    !message.content.startsWith(">")
-  ) {
-    const args = message.content.trim().split(/\s+/); // Split message content into args
-    try {
-      // Trigger the GPT command directly
-      await gpt(client, message, args);
-    } catch (err) {
-      console.error("Error triggering GPT command:", err);
+    if (!message.guild) return;
+    if (message.author === client.user) return;
+    if (message.author.bot) return;
+    const gptChannelId = "1328187329344442399";
+    const guilddb = await guildSchema.findOne({ _id: message.guild.id });
+    let guildPrefix = guilddb ? guilddb.prefix : defaultPrefix;
+    // Handle events for specific channel
+    if (
+        message.channel.id === gptChannelId &&
+        !message.content.startsWith(guildPrefix) &&
+        !message.content.startsWith(">")
+    ) {
+        const args = message.content.trim().split(/\s+/); // Split message content into args
+        try {
+            // Trigger the GPT command directly
+            await gpt(client, message, args);
+        } catch (err) {
+            console.error("Error triggering GPT command:", err);
+        }
+        return; // Prevent further processing for this message
     }
-    return; // Prevent further processing for this message
-  }
-  msgevents(message);
+    msgevents(message);
 
-  if (message.content.startsWith("<@1032039037990600766>")) {
-    message.channel.send({
-      embeds: [
-        {
-          color: 0xfffffe,
-          description: `Use server prefix \`${guildPrefix}\` to call commands`,
-        },
-      ],
-    });
-  }
-
-  if (!message.content.startsWith(guildPrefix)) return;
-
-  const args = message.content
-    .slice(guildPrefix.length)
-    .split(/\s+/)
-    .filter(Boolean);
-
-  for (const [category, cat_commands] of Object.entries(cmdlist)) {
-    for (const [key, value] of Object.entries(cat_commands)) {
-      command(client, value, eval(key), message, args);
+    if (message.content.startsWith("<@1032039037990600766>")) {
+        message.channel.send({
+            embeds: [
+                {
+                    color: 0xfffffe,
+                    description: `Use server prefix \`${guildPrefix}\` to call commands`,
+                },
+            ],
+        });
     }
-  }
+    if (message.channel.id != gptChannelId) {
+        return;
+    }
+    if (!message.content.startsWith(guildPrefix)) return;
+
+    const args = message.content
+        .slice(guildPrefix.length)
+        .split(/\s+/)
+        .filter(Boolean);
+
+    for (const [category, cat_commands] of Object.entries(cmdlist)) {
+        for (const [key, value] of Object.entries(cat_commands)) {
+            command(client, value, eval(key), message, args);
+        }
+    }
 });
 
 client.on("guildCreate", function (guild) {
-  joinguild(guild);
+    joinguild(guild);
 });
 
 client.login(process.env.TOKEN);
